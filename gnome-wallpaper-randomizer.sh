a258b627a503bd1f3395a3e3d8b2dad3cea99a37
Omar Morelos <>
2022-08-10 22:10:25 +0800

    No longer using a random array index as it doesn't guarantee all images would be shown.

--- gnome-wallpaper-randomizer.sh
+++ gnome-wallpaper-randomizer.sh
@@ -15,12 +15,28 @@ fi
 
 IMAGES=("$1"/*)
 
+# Check current image-index. If it doesn't exist, it will be initalized to 0 later.
+CURRENT_BG_INDEX=0
+if [[ -e image-index ]]
+then
+    CURRENT_BG_INDEX=$(cat image-index)
+fi
+
+# Go back to first file if reached the last one.
+if [[ $(($CURRENT_BG_INDEX + 1)) -gt ${#IMAGES[@]} ]]
+then
+    CURRENT_BG_INDEX=0
+fi
+
 # Load Gnome session.
 RUID=$(id -u -r)
 PID=($(pgrep -u $RUID gnome-session))
 # PID could potentially be more than one so any will do for now.
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${PID[1]}/environ| tr '\0' '\n' | cut -d= -f2-)
-RND_BG="${IMAGES[ $RANDOM % ${#IMAGES[@]} ]}"
+RND_BG="${IMAGES[$CURRENT_BG_INDEX]}"
+
+# Write latest current index to a temp file
+echo $(($CURRENT_BG_INDEX+1)) > image-index
 
 # As of Ubuntu 22.04, the following check must be applied.
 PICTURE_URI=picture-uri
